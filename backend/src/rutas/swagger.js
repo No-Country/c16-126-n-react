2ccd23d974614e1adb6030e0328eeb6e99180cbd
Franco_Ojeda <24martin94@gmail.com>
2024-02-19 21:04:03 -0300

    Modificacion de swagger para que funciona la vaina

--- backend/src/rutas/swagger.js
+++ backend/src/rutas/swagger.js
@@ -1,28 +1,28 @@
-const swaggerJSDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-const path = require('path')
+// const swaggerJSDoc = require('swagger-jsdoc')
+// const swaggerUi = require('swagger-ui-express')
+// const path = require('path')
 
-const opciones = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Api de Servicios',
-      version: '1.0.0',
-    },
-  },
-  apis: [`${path.join(__dirname, './rutas.js')}`],
-}
+// const opciones = {
+//   swaggerDefinition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Api de Servicios',
+//       version: '1.0.0',
+//     },
+//   },
+//   apis: [`${path.join(__dirname, './rutas.js')}`],
+// }
 
-const swaggerSpec = swaggerJSDoc(opciones)
+// const swaggerSpec = swaggerJSDoc(opciones)
 
 
-const swaggerDocs = (app, port) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-  app.use('/docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.send(swaggerSpec)
-  })
-}
-// console.log(swaggerSpec);
-// console.log(opciones);
-module.exports = { swaggerDocs }
+// const swaggerDocs = (app, port) => {
+//   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+//   app.use('/docs.json', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json')
+//     res.send(swaggerSpec)
+//   })
+// }
+// // console.log(swaggerSpec);
+// // console.log(opciones);
+// module.exports = { swaggerDocs }

6ae0d80dce8728beae3916ed133439a5bcdeea06
Eduardo Hidalgo <eduardohidalgo0210@gmail.com>
2024-02-22 14:24:45 -0400

    servicios page diseñada

--- frontend/app-servicios-profesionales/src/components/LandingPage/LandingPage.jsx
+++ frontend/app-servicios-profesionales/src/components/LandingPage/LandingPage.jsx
@@ -6,38 +6,38 @@ import Servicios from "../Servicios/Servicios";
 const LandingPage = () => {
   return (
     <>
-      <div class="bg-cover bg-center h-auto text-white py-24 px-10 bg-blue-300 flex items-center justify-center">
-        <div class="md:w-3/4 lg:w-1/2 text-center">
-          <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
+      <div className="bg-cover bg-center h-auto text-white py-24 px-10 bg-blue-300 flex items-center justify-center">
+        <div className="md:w-3/4 lg:w-1/2 text-center">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
             Busca un profesional <br /> o ofrece tus servicios
           </h1>
-          <p class="text-lg md:text-xl text-white font-bold mb-10 leading-relaxed">
+          <p className="text-lg md:text-xl text-white font-bold mb-10 leading-relaxed">
             Encuentra profesionales confiables para tus necesidades o da a
             conocer tus servicios al mundo.
           </p>
           <a
             href="#"
-            class="bg-blue-800 py-4 px-8 text-white font-bold uppercase text-sm md:text-base rounded hover:bg-blue-600 transition-colors duration-300 ease-in-out"
+            className="bg-blue-800 py-4 px-8 text-white font-bold uppercase text-sm md:text-base rounded hover:bg-blue-600 transition-colors duration-300 ease-in-out"
           >
             Busca un profesional
           </a>
         </div>
       </div>
 
-      <section class="flex justify-center items-center flex-col mt-20">
-        <div class="w-full md:w-auto p-4">
-          <p class="text-3xl text-blue-700/80 font-bold text-wrap">
+      <section className="flex justify-center items-center flex-col mt-20">
+        <div className="w-full md:w-auto p-4">
+          <p className="text-3xl text-blue-700/80 font-bold text-wrap">
             Un profesional cualificado en algunos clicks
           </p>
 
-          <p class="mt-2 text-[20px]  font-semibold ">
+          <p className="mt-2 text-[20px]  font-semibold ">
             Puedes encontrar un profesional altamente cualificado y de confianza
             <br /> ingresando a nuestra plataforma
           </p>
         </div>
-        <div class="max-w-screen-lg grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8 mt-10">
-          <div class="w-full md:w-auto p-4">
-            <h3 class="text-xl font-bold mb-2 text-blue-700/90">
+        <div className="max-w-screen-lg grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8 mt-10">
+          <div className="w-full md:w-auto p-4">
+            <h3 className="text-xl font-bold mb-2 text-blue-700/90">
               Ofrece tus servicios como profesional
             </h3>
             <p className="text-[17px] font-semibold">
@@ -45,8 +45,8 @@ const LandingPage = () => {
               nuestra plataforma
             </p>
           </div>
-          <div class="w-full md:w-auto p-4">
-            <h3 class="text-xl font-bold mb-2 text-blue-700/90">
+          <div className="w-full md:w-auto p-4">
+            <h3 className="text-xl font-bold mb-2 text-blue-700/90">
               Contacta un profesional
             </h3>
             <p className="text-[17px] font-semibold">
@@ -55,8 +55,8 @@ const LandingPage = () => {
               clicks.
             </p>
           </div>
-          <div class="w-full md:w-auto p-4">
-            <h3 class="text-xl font-bold mb-2 text-blue-700/90">
+          <div className="w-full md:w-auto p-4">
+            <h3 className="text-xl font-bold mb-2 text-blue-700/90">
               Te facilitamos la vida
             </h3>
             <p className="text-[17px] font-semibold">

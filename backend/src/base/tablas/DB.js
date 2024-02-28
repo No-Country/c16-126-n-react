4d815143f4813b9f6f514177d2acbc351a8464f6
Franco_Ojeda <24martin94@gmail.com>
2024-02-28 18:01:22 -0300

    Cambios de variables de entorno

--- backend/src/base/tablas/DB.js
+++ backend/src/base/tablas/DB.js
@@ -1,14 +1,21 @@
 // ESTE ARCHIVO CORRESPONDE A LA NUEVA BASE DE DATOS DE MYSQL
 
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+  DATABASE
+} = require('../../config')
 const { createPool } = require('mysql2/promise')
 
 
 const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '37Franco57#',
-  database: 'allservicesapi',
-  port: 3306
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DATABASE,
+  port: DB_PORT
 })
 
 module.exports = { pool }

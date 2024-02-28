2d929c5c8d319eb8d31725b76438d61f6e3ea3da
Franco_Ojeda <24martin94@gmail.com>
2024-02-28 17:57:25 -0300

    cambios en las variables de entorno

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,5 @@
 const { config } = require('dotenv')
-
 config()
-
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || '37Franco57#'

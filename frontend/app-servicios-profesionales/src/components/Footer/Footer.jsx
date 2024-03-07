4fd1244aeac1f420090ec3a76b418984b7abbc48
Eduardo Hidalgo <eduardohidalgo0210@gmail.com>
2024-03-07 09:05:53 -0400

    boton registrarse footer corregido

--- frontend/app-servicios-profesionales/src/components/Footer/Footer.jsx
+++ frontend/app-servicios-profesionales/src/components/Footer/Footer.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../auth/AuthContext";
+
+
+
 
 const Footer = () => {
+  const {logged} = useContext(AuthContext)
   return (
     <footer className="flex flex-col items-center text-center text-white bg-blue-600 mt-auto">
       <div className="container p-6">
         <p className="flex items-center justify-center">
           <span className="mr-4">Registrate gratis</span>
-          <button
-            type="button"
+          <NavLink
+            to={logged? "/perfil" : "/register"}
             className="inline-block rounded-full border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
             data-te-ripple-init
             data-te-ripple-color="light"
           >
-            <NavLink to="/register">Registrarse</NavLink>
-          </button>
+            Registrarse
+          </NavLink>
         </p>
       </div>
 
